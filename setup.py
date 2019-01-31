ee8a5da4d74c2da29b1cf73049250fd5609f1f43
Maria Prudyvus <maria.prudyvus@gmail.com>
2019-01-31 19:31:31 +0300

    old python

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-
+import codecs
 import os
 from setuptools import setup, find_packages
 
@@ -17,7 +17,7 @@ VERSION = "1.{}".format(os.getenv('BUILD_NUMBER', '0'))
 REQUIRES = ["connexion"]
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with codecs.open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
