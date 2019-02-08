b268fac859c2fecb8c1dbf388ad454859ca005e7
Maria Prudyvus <maria.prudyvus@gmail.com>
2019-02-08 18:49:55 +0300

    import order

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+import os
 
-from swagger_server import config, encoder
+from swagger_server import encoder
+from swagger_server.config import app, settings
 
-config.app.app.json_encoder = encoder.JSONEncoder
-config.app.add_api('swagger.yaml', arguments={'title': 'Maintenance'})
+app.app.json_encoder = encoder.JSONEncoder
+specification = os.path.join(os.path.join(os.path.dirname(__file__), 'swagger'), 'swagger.yaml')
+app.add_api('specification', arguments={'title': 'Maintenance'})
 
 if __name__ == '__main__':
-    config.app.run(port=config.settings.APP_PORT)
+    app.run(port=settings.APP_PORT)
