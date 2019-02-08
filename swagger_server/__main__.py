25ddb9216263786ab7dc1efe27ae11f4bfba981a
Maria Prudyvus <maria.prudyvus@gmail.com>
2019-02-08 18:57:41 +0300

    fix variable

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -6,7 +6,7 @@ from swagger_server.config import app, settings
 
 app.app.json_encoder = encoder.JSONEncoder
 specification = os.path.join(os.path.join(os.path.dirname(__file__), 'swagger'), 'swagger.yaml')
-app.add_api('specification', arguments={'title': 'Maintenance'})
+app.add_api(specification, arguments={'title': 'Maintenance'})
 
 if __name__ == '__main__':
     app.run(port=settings.APP_PORT)
